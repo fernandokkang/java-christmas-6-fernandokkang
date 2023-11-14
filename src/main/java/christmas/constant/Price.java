1a62276e11528f8eac2286d9a6632aa5e0e00436
fernandokkang <spineattack@gmail.com>
2023-11-14 17:42:30 +0900

    feat. 가격 관련 상수 클래스 구성

---
+++ src/main/java/christmas/constant/Price.java
@@ -0,0 +1,14 @@
+package christmas.constant;
+
+public class Price {
+
+    public static int WEEKDAY_DESSERT_DISCOUNT_PRICE = 2023;
+    public static int WEEKEND_MAIN_MENU_DISCOUNT_PRICE = 2023;
+    public static int SPECIAL_DISCOUNT_PRICE = 1000;
+    public static int DEFAULT_D_DAY_DISCOUNT_PRICE = 1000;
+    public static int MINIMUM_PRICE_APPLY_EVENT = 10000;
+    public static int MINIMUM_PRICE_RECEIVE_GIFT = 120000;
+    public static int MINIMUM_BENEFIT_PRICE_RECEIVE_STAR_BADGE = 5000;
+    public static int MINIMUM_BENEFIT_PRICE_RECEIVE_TREE_BADGE = 10000;
+    public static int MINIMUM_BENEFIT_PRICE_RECEIVE_SANTA_BADGE = 20000;
+}
