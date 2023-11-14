76352c17cacbb84825e8223f9f7eac4a47e0aa30
fernandokkang <spineattack@gmail.com>
2023-11-14 23:37:42 +0900

    feat. 뱃지 출력 테스트 코드

---
+++ src/test/java/christmas/domainTest/BadgeTest.java
@@ -0,0 +1,54 @@
+package christmas.domainTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.Badge;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BadgeTest {
+
+    @DisplayName("뱃지 출력 확인 산타")
+    @Test
+    void printBadgeTest_SANTA() {
+
+        int benefitPrice = 30000;
+
+        Badge badge = Badge.giveBadge(benefitPrice);
+
+        assertThat(badge.getType()).isEqualTo("산타");
+    }
+
+    @DisplayName("뱃지 출력 확인 트리")
+    @Test
+    void printBadgeTest_TREE() {
+
+        int benefitPrice = 15000;
+
+        Badge badge = Badge.giveBadge(benefitPrice);
+
+        assertThat(badge.getType()).isEqualTo("트리");
+    }
+
+    @DisplayName("뱃지 출력 확인 스타")
+    @Test
+    void printBadgeTest_STAR() {
+
+        int benefitPrice = 5000;
+
+        Badge badge = Badge.giveBadge(benefitPrice);
+
+        assertThat(badge.getType()).isEqualTo("별");
+    }
+
+    @DisplayName("뱃지 출력 확인 없음")
+    @Test
+    void printBadgeTest_EMPTY() {
+
+        int benefitPrice = 0;
+
+        Badge badge = Badge.giveBadge(benefitPrice);
+
+        assertThat(badge.getType()).isEqualTo("없음");
+    }
+}
