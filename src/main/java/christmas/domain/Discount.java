854945d98160386557ed586a3ad5ce72af51ab30
fernandokkang <spineattack@gmail.com>
2023-11-14 17:41:13 +0900

    feat. Discount 객체 구성 및 테스트 코드 작성

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -1,4 +1,87 @@
 package christmas.domain;
 
-public class Discount {
+import christmas.constant.Price;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.*;
+
+public enum Discount {
+
+    WEEKDAY(Arrays.asList("SUN","MON","TUE","WED","THU"), Price.WEEKDAY_DESSERT_DISCOUNT_PRICE),
+    WEEKEND(Arrays.asList("FRI","SAT"), Price.WEEKEND_MAIN_MENU_DISCOUNT_PRICE),
+    SPECIAL(Arrays.asList("3","10","17","24","25","31"), Price.SPECIAL_DISCOUNT_PRICE),
+    EMPTY(Collections.EMPTY_LIST, 0);
+
+    private List<String> dateList;
+    private int price;
+
+    private Discount(List<String> dateList, int price) {
+
+        this.dateList = dateList;
+        this.price = price;
+    }
+    public static Map<String, Integer> getDiscountEventOptions(String date){
+
+        String [] dayOfWeekEng = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
+
+        LocalDate localDateDate = LocalDate.of(2023, 12, Integer.parseInt(date));;
+        DayOfWeek dayOfWeek = localDateDate.getDayOfWeek();
+
+        String day = dayOfWeekEng[dayOfWeek.getValue() - 1];
+
+        return makeEventOptions(date, day);
+    }
+    private static Map<String, Integer> makeEventOptions(String date, String day) {
+
+        Map<String, Integer> options = new HashMap<>();
+
+        options.put("크리스마스 디데이 할인",DDayDiscountPrice(date));
+        options.put("평일 할인",findWeekdayDiscountPrice(day));
+        options.put("주말 할인",findWeekendDiscountPrice(day));
+        options.put("특별 할인",findSpecialDiscountPrice(date));
+
+        return options;
+    }
+
+    private static int DDayDiscountPrice(String date) {
+
+        int parseDate = Integer.parseInt(date);
+
+        if(parseDate > 25) {
+            return 0;
+        }
+
+        return (parseDate-1)*100 + Price.DEFAULT_D_DAY_DISCOUNT_PRICE;
+    }
+
+    private static int findWeekendDiscountPrice(String day) {
+
+        for(String s : WEEKEND.dateList) {
+            if(s.equals(day)) {
+                return WEEKEND.price;
+            }
+        }
+        return EMPTY.price;
+    }
+
+    private static int findWeekdayDiscountPrice(String day) {
+
+        for(String s : WEEKDAY.dateList) {
+            if(s.equals(day)) {
+                return WEEKDAY.price;
+            }
+        }
+        return EMPTY.price;
+    }
+
+    private static int findSpecialDiscountPrice(String date) {
+
+        for (String s : SPECIAL.dateList) {
+            if (s.equals(date)){
+                return SPECIAL.price;
+            }
+        }
+        return EMPTY.price;
+    }
 }
---
+++ src/test/java/christmas/domainTest/DiscountTest.java
@@ -0,0 +1,122 @@
+package christmas.domainTest;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.constant.Price;
+import christmas.domain.Discount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Map;
+
+public class DiscountTest {
+
+    @Test
+    @DisplayName("날짜 입력하면 해당하는 할인 출력 평일할인")
+    public void findDiscountToApplyTest_Weekdays() {
+
+        String date = "4";
+
+        Map<String, Integer> options =
+                Discount.getDiscountEventOptions(date);
+
+        assertThat(options).containsEntry("크리스마스 디데이 할인",1300)
+                .containsEntry("주말 할인",0)
+                .containsEntry("평일 할인",Price.WEEKDAY_DESSERT_DISCOUNT_PRICE)
+                .containsEntry("특별 할인",0);
+    }
+
+    @Test
+    @DisplayName("날짜 입력하면 해당하는 할인 출력 평일, 특별할인")
+    public void findDiscountToApplyTest_Weekdays_Special() {
+
+        String date = "10";
+
+        Map<String, Integer> options =
+        Discount.getDiscountEventOptions(date);
+
+        assertThat(options).containsEntry("크리스마스 디데이 할인",1900)
+                .containsEntry("주말 할인",0)
+                .containsEntry("평일 할인",Price.WEEKDAY_DESSERT_DISCOUNT_PRICE)
+                .containsEntry("특별 할인",Price.SPECIAL_DISCOUNT_PRICE);
+    }
+
+    @Test
+    @DisplayName("날짜 입력하면 해당하는 할인 출력 주말할인")
+    public void findDiscountToApplyTest_Weekend() {
+
+        String date = "9";
+
+        Map<String, Integer> options =
+                Discount.getDiscountEventOptions(date);
+
+        assertThat(options).containsEntry("크리스마스 디데이 할인",1800)
+                .containsEntry("주말 할인",Price.WEEKEND_MAIN_MENU_DISCOUNT_PRICE)
+                .containsEntry("평일 할인",0)
+                .containsEntry("특별 할인",0);
+    }
+
+    @Test
+    @DisplayName("날짜 입력하면 해당하는 할인 출력 크리스마스 당일")
+    public void findDiscountToApplyTest_Christmas() {
+
+        String date = "25";
+
+        Map<String, Integer> options =
+                Discount.getDiscountEventOptions(date);
+
+        assertThat(options).containsEntry("크리스마스 디데이 할인",3400)
+                .containsEntry("주말 할인",0)
+                .containsEntry("평일 할인",Price.WEEKDAY_DESSERT_DISCOUNT_PRICE)
+                .containsEntry("특별 할인",Price.SPECIAL_DISCOUNT_PRICE);
+    }
+
+    @Test
+    @DisplayName("날짜 입력하면 해당하는 할인 출력 크리스마스 이후 평일")
+    public void findDiscountToApplyTest_AfterChristmas_Weekday() {
+
+        String date = "26";
+
+        Map<String, Integer> options =
+                Discount.getDiscountEventOptions(date);
+
+        assertThat(options).containsEntry("크리스마스 디데이 할인",0)
+                .containsEntry("주말 할인",0)
+                .containsEntry("평일 할인",Price.WEEKDAY_DESSERT_DISCOUNT_PRICE)
+                .containsEntry("특별 할인",0);
+    }
+
+    @Test
+    @DisplayName("날짜 입력하면 해당하는 할인 출력 크리스마스 이후 평일 특별 할인")
+    public void findDiscountToApplyTest_AfterChristmas_Weekday_Special() {
+
+        String date = "27";
+
+        Map<String, Integer> options =
+                Discount.getDiscountEventOptions(date);
+
+        assertThat(options).containsEntry("크리스마스 디데이 할인",0)
+                .containsEntry("주말 할인",0)
+                .containsEntry("평일 할인",Price.WEEKDAY_DESSERT_DISCOUNT_PRICE)
+                .containsEntry("특별 할인",0);
+    }
+
+    @Test
+    @DisplayName("날짜 입력하면 해당하는 할인 출력 크리스마스 이후 주말")
+    public void findDiscountToApplyTest_AfterChristmas_Weekend() {
+
+        String date = "31";
+
+        Map<String, Integer> options =
+                Discount.getDiscountEventOptions(date);
+
+        assertThat(options).containsEntry("크리스마스 디데이 할인",0)
+                .containsEntry("주말 할인",0)
+                .containsEntry("평일 할인",Price.WEEKDAY_DESSERT_DISCOUNT_PRICE)
+                .containsEntry("특별 할인",Price.SPECIAL_DISCOUNT_PRICE);
+    }
+}
